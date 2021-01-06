2779c63f81bff0703d52f2619126b469fda4a3a0
Lucas Dumestre <lucas.dumestre@wanadoo.fr>
2021-01-06 14:57:59 +0100

    New API to get Covid infos in Loire-Atlantique

--- API/main.py
+++ API/main.py
@@ -1,10 +1,24 @@
 from flask import Flask
+import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return 'Hello there!'
+
+@app.route('/covid')
+def covid_controller():
+    uri = "https://coronavirusapi-france.now.sh/LiveDataByDepartement?Departement=Loire-Atlantique"
+    try:
+        uResponse = requests.get(uri)
+    except requests.ConnectionError:
+        return "Connection Error"
+    Jresponse = uResponse.text
+    data = json.loads(Jresponse)
+
+    return data["LiveDataByDepartement"][0]
 
 
 if __name__ == "__main__":
