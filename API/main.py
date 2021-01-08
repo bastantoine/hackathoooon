29bf75aab7a2102cea43782ae8c83874e0fd400c
bastantoine <bastien.antoine69+github@gmail.com>
2021-01-08 19:08:24 +0100

    Added some specific code to be able to run the app on Scalingo
    Due to technical limitations, we need to serve the static files, including those built from React, using our API (...).
    To do so we dynamically create the routes for each static file.
    
    We also have a bit of code specifi to how Scalingo runs a Flask app.

--- API/main.py
+++ API/main.py
@@ -1,4 +1,9 @@
-from flask import Flask
+import os
+
+from flask import (
+    Flask,
+    send_from_directory,
+)
 from flask_cors import CORS
 import requests
 import json
@@ -23,5 +28,51 @@ def covid_controller():
     return data["LiveDataByDepartement"][0]
 
 
+############################
+# ROUTES FOR SCALINGO ONLY #
+############################
+
+# Before the deployement, a GitHub Action job will build the React app, and copy the files in
+# API/static. Then the routes below will be used to serve the files without needing any nginx
+# running.
+
+def view_maker(directory, filename):
+    # Helper used to create the views used to serve the static files
+    # From: https://stackoverflow.com/a/13734321/10104112
+    return lambda: send_from_directory(directory=directory, filename=filename)
+
+def setup_routes_for_scalingo():
+    create_route_for_files_of_folder('./build')
+
+def create_route_for_files_of_folder(path):
+    for entry in os.listdir(path):
+        filename = os.path.join(path, entry)
+        if os.path.isfile(filename):
+            # We have a file, let's create a route to serve it under it's own path, so a file stored
+            # at build/static/css/my_file.css wwould be served under /static/css/my_file.css
+            app.add_url_rule(
+                '/' + filename.replace('./build/', ''),
+                endpoint=filename.replace('./build/', ''),
+                view_func=view_maker(path, entry)
+            )
+        elif os.path.isdir(filename):
+            # We have a folder, let's create the routes for all it's files
+            create_route_for_files_of_folder(filename)
+
+############################
+
 if __name__ == "__main__":
-    app.run()
+    try:
+        DEPLOY_SCALINGO = bool(int(os.environ.get('DEPLOY_SCALINGO', 0)))
+    except ValueError:
+        DEPLOY_SCALINGO = False
+    if DEPLOY_SCALINGO:
+        # We are deploying on Scalingo, let's create the routes for the static files
+        setup_routes_for_scalingo()
+        try:
+            port = int(os.environ.get("PORT", 5000))
+        except ValueError:
+            port = 5000
+        app.run(host='0.0.0.0', port=port)
+    else:
+        app.run()
