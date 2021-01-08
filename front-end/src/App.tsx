f0308802edb2123f0e17bd99efdfad5932806042
Lucas Dumestre <lucas.dumestre@wanadoo.fr>
2021-01-08 11:43:38 +0100

    Some change on front end to test the connection to the backend
    The front end get from backend api the covid info for Loire-Atlantique and log the data in console

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,13 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  const apiUrl = 'http://127.0.0.1:5000/covid';
+  let data = "test";
+  fetch(apiUrl)
+      .then((response) => response.json())
+      .then((response_data) => {
+        console.log('nouvellesHospitalisations', response_data.nouvellesHospitalisations);
+        data = response_data;
+      });
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <img src="https://i.kym-cdn.com/entries/icons/original/000/029/079/hellothere.jpg" />
+          <br/>
+          Hello there !
         </p>
+        <div className="covid">
+          <p className="covidH">{data}</p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
