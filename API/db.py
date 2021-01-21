4a3cc186207b05aa033b17f51d8b48ef992e5c78
bastantoine <bastien.antoine69+github@gmail.com>
2021-01-21 21:25:24 +0100

    Setup DB with SQLITE

---
+++ API/db.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+from flask import (
+    g,
+)
+
+from main import init_app
+
+app = init_app()
+DATABASE = "./database.sqlite"
+
+# From https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
