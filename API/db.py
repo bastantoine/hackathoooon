a22153b187b600332e0f31c2e7c6e2f85a2ae556
bastantoine <bastien.antoine69+github@gmail.com>
2021-01-22 00:33:17 +0100

    Do not use anything related to flask to connect to the DB
    This way, we can use the connection in regular python files without having to use an app context.

--- API/db.py
+++ API/db.py
@@ -1,12 +1,5 @@
 import sqlite3
 
-from flask import (
-    g,
-)
-
-from main import init_app
-
-app = init_app()
 DATABASE = "./database.sqlite"
 
 # From https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
@@ -16,17 +9,9 @@ def make_dicts(cursor, row):
                 for idx, value in enumerate(row))
 
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+    db = sqlite3.connect(DATABASE)
     db.row_factory = sqlite3.Row
     return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
 
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
@@ -40,16 +25,14 @@ def insert(table, params):
         columns.append(key)
         values.append(f"'{value}'")
     query = f"INSERT INTO {table} ({', '.join(columns)}) VALUES ({', '.join(values)})"
-    # print(f"Running {query}")
-    with app.app_context():
-        db = get_db()
-        db.cursor().execute(query)
-        db.commit()
-        db.close()
+    db = get_db()
+    db.cursor().execute(query)
+    db.commit()
+    db.close()
 
 def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    db = get_db()
+    with open('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
