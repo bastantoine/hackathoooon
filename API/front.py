4f64065df5ca705ae3399fafd3715d297679c993
Lucas Dumestre <lucas.dumestre@wanadoo.fr>
2021-01-21 18:20:11 +0100

    Modify front to get and display the next scheduled class

--- API/front.py
+++ API/front.py
@@ -3,7 +3,7 @@ from flask import render_template, Blueprint
 from dotenv import load_dotenv
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -11,21 +11,37 @@ front = Blueprint('front', __name__, template_folder='templates')
 @front.route('/')
 def template_test():
     # We get the user information from our mocked API
-    user = requests.get(os.path.join(os.environ.get('API_URL', ''), 'mock/pass/calendar?user=1'))
-    user_calendar = json.loads(user.text)['calendar']
+    user_data = requests.get(os.path.join(os.environ.get('API_URL', ''), 'mock/pass/calendar?user=1'))
 
-    # We search the next_course
-    today = datetime.today()
-    today_course = {}
-    while today_course == "":
-        try:
-            # We try to find a course for today, if we don't find => fail
-            today_course = user_calendar[today.strftime('%A')]
-            break
-        except:
-            # We pass to the next day
-            today += datetime.timedelta(days=1)
-            break
+    course = get_current_or_next_course(user_data)
 
+    return render_template('template.html', course=course)
 
-    return render_template('template.html', today=today_course)
+def get_current_or_next_course(user_data):
+    user_calendar = json.loads(user_data.text)['calendar']
+
+    # We search for the next course
+    now = datetime.now()
+    today = now.strftime('%A')
+
+    # First try to get the courses of today
+    today_courses = user_calendar.get(today, [])
+
+    if today_courses:
+        # We have one or more classes this day
+        next_or_current_courses = list(filter(
+            lambda course: now < datetime.strptime(course['end'], '%H:%M'),
+            today_courses
+        ))
+        if next_or_current_courses:
+            # Return the next or current course left in the day
+            return next_or_current_courses[0]
+
+    next_day = now
+    while next_day < next_day + timedelta(days=7):
+        next_day += timedelta(days=1)
+        this_day_courses = user_calendar.get(next_day.strftime('%A'), [])
+        if this_day_courses:
+            return this_day_courses[0]
+
+    return {}
