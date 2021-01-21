ae29c8bfc3c4266f681d3952a913b4e3e3af1c16
Lucas Dumestre <lucas.dumestre@wanadoo.fr>
2021-01-21 14:58:02 +0100

    Add comments in front code

--- API/front.py
+++ API/front.py
@@ -6,16 +6,20 @@ from datetime import datetime
 front = Blueprint('front', __name__, template_folder='templates')
 @front.route('/')
 def template_test():
+    # We get the user information from our mocked API
     user = requests.get('https://hackathoooon.osc-fr1.scalingo.io/mock/pass/calendar?user=1')
     user_calendar = json.loads(user.text)['calendar']
 
+    # We search the next_course
     today = datetime.today()
     today_course = ""
     while today_course == "":
         try:
+            # We try to find a course for today, if we don't find => fail
             today_course = user_calendar[today.strftime('%A')]
             break
         except:
+            # We pass to the next day
             today += datetime.timedelta(days=1)
             break
 
