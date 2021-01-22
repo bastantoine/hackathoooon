dd11b18393dc28ce149abedc737dce729d24121b
Vencesdab <56306669+Vencesdab@users.noreply.github.com>
2021-01-22 10:00:37 +0100

    Update geometry.py

--- API/views/geometry.py
+++ API/views/geometry.py
@@ -1,1 +1,13 @@
 #retourne 3 fichier, un pour construire les salles du rdc, un pour le 1er étage, un pour les contours du bâtiment
+import json
+
+from flask import (Blueprint)
+
+geometry = Blueprint('geometry', __name__)
+
+@geometry.route('/geometry/floor')
+def geojson_floors():
+    from db import query_db
+    query = "SELECT name, geometry, room_type FROM rooms"
+    magic_list = query_db(query)
+    print(magic_list)
