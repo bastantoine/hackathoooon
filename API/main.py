e30731e42810ab993ceeb73b5074feae7fe1d48f
bastantoine <bastien.antoine69+github@gmail.com>
2021-01-28 11:42:21 +0100

    Populate DB on creation of the app

--- API/main.py
+++ API/main.py
@@ -1,9 +1,32 @@
+import json
 import os
 
 from flask import Flask
 from flask_cors import CORS
 
 import db
+from views.rooms import insert_features_in_db
+
+def init_db():
+    # Make sure we have the tables up-to-date in the DB
+    db.init_db()
+
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    main_dir = os.path.join(SITE_ROOT, "views", "resources", "GEOJSON")
+    # Some GeoJSON files are not meant to be used in the graph, and thus should not be imported in
+    # the DB. Don't even try, this will raise an error
+    excluded_filenames = [
+        'CONTOUR.geojson'
+    ]
+    filenames = [
+        filename for filename in os.listdir(main_dir)
+        if filename.endswith('.geojson')
+           and filename not in excluded_filenames
+    ]
+    for filename in filenames:
+        with open(os.path.join(main_dir, filename)) as file:
+            insert_features_in_db(filename, json.loads(file.read()))
+            print(f"Inserted features of {filename}")
 
 def init_app():
     """Initialize the core application."""
@@ -11,6 +34,8 @@ def init_app():
 
     # Initialize Plugins
     CORS(app)
+
+    init_db()
 
     with app.app_context():
         # Include our Routes
@@ -29,6 +54,7 @@ def init_app():
 
         return app
 
+
 if __name__ == "__main__":
     app = init_app()
     try:
@@ -40,7 +66,6 @@ if __name__ == "__main__":
             port = int(os.environ.get("PORT", 5000))
         except ValueError:
             port = 5000
-        # db.init_db()
         app.run(host='0.0.0.0', port=port)
     else:
         app.run()
